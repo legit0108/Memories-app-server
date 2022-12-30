60d88163af2bdc0924450b301995e9e7ad425bc5
legit0108 <legitgamer2345@gmail.com>
2022-12-31 00:47:22 +0530

    Revert back to nodemailer

--- utility/nodemailer.js
+++ utility/nodemailer.js
@@ -1,43 +1,15 @@
-// import nodemailer from 'nodemailer'
-
-import sendgridMail from '@sendgrid/mail'
+import nodemailer from 'nodemailer'
 
 export const sendMail = async(str, userName, email, link="")=>{
-    // let transporter = nodemailer.createTransport({
-    //    host: "smtp.gmail.com",
-    //    port: 587,
-    //    secure: false,
-    //    auth: {
-    //       user: process.env.USER,
-    //       pass: process.env.PASSWORD
-    //    }
-    // })
-
-    // var subject, text, html;
-       
-    // if(str==="signup"){    
-    //     subject =`Thank you for signing up, ${userName}`;
-    //     html=`
-    //     <h1>WELCOME TO MEMORIES, ${userName.toUpperCase()}!</h1>
-    //     Hope you have a good time!`
-    // }else if(str==="resetPassword"){
-    //     subject = `Reset password link`
-    //     html=`
-    //     <h1>MEMORIES</h1>
-    //     Hello ${userName}, here is your link to reset your password: ${link} .
-    //     Note that this is a one time link and will expire in 15 minutes.`
-    // }
-
-    // await new Promise((resolve, reject)=>{
-    //     transporter.sendMail({
-    //         from: process.env.USER,
-    //         to: email,
-    //         subject,
-    //         html
-    //     })
-    // })
-
-    sendgridMail.setApiKey(process.env.SENDGRID_API_KEY)
+    let transporter = nodemailer.createTransport({
+       host: "smtp.gmail.com",
+       port: 587,
+       secure: false,
+       auth: {
+          user: process.env.USER,
+          pass: process.env.PASSWORD
+       }
+    })
 
     var subject, text, html;
        
@@ -54,17 +26,12 @@ export const sendMail = async(str, userName, email, link="")=>{
         Note that this is a one time link and will expire in 15 minutes.`
     }
 
-    const messageData = {
-        to: email,
-        from: 'lakshitpandey02@gmail.com',
-        subject,
-        html
-    }
-
-    try{
-        await sendgridMail.send(messageData)
-        console.log('message sent')
-    }catch(error){
-        console.log(error)
-    }
+    await new Promise((resolve, reject)=>{
+        transporter.sendMail({
+            from: process.env.USER,
+            to: email,
+            subject,
+            html
+        })
+    })
 }
